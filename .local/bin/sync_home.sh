4d8ac9d8c0d0c154b7a1a3c48207f08be62f1a8f
Theo Brigitte <theo.brigitte@gmail.com>
2024-06-27 23:49:08 +0200

    sync_home: add logs, add additional args, enable stats, enable humand readble sizes

--- .local/bin/sync_home.sh
+++ .local/bin/sync_home.sh
@@ -20,12 +20,29 @@ exit_error() {
 # Helper function - shows the usage message and exits
 usage() {
     script_name="$(basename "$0")"
-    echo_stderr "Usage: ${script_name} target"
+    echo_stderr "Usage: ${script_name} target -- [rsync options]"
     echo_stderr "  This is a wrapper around rsync to help backup home directory."
     echo_stderr ""
     echo_stderr "  It does sync $SOURCE_DIR to the given target"
     echo_stderr "  It excludes files listed in $CONFIG_DIR/excludes"
     echo_stderr "  It logs the output to $LOG_DIR/YYYYMMDD_HHMMSS.log"
+    echo_stderr ""
+    echo_stderr "Arguments:"
+    echo_stderr "  target: the destination directory to sync to"
+    echo_stderr "          can be a local path or a remote path in the form of user@host:path"
+    echo_stderr "  --: separates the target from the rsync options"
+    echo_stderr "  rsync options: additional options to pass to rsync"
+    echo_stderr ""
+    echo_stderr "Options:"
+    echo_stderr "  -h: show this help message"
+    echo_stderr ""
+    echo_stderr "Example:"
+    echo_stderr "  ${script_name} /media/backup/"
+    echo_stderr "  ${script_name} remote_host:/media/backup/"
+    echo_stderr "  ${script_name} /media/backup/ -- --dry-run"
+    echo_stderr ""
+    echo_stderr "Note:"
+    echo_stderr "  The source directory is hardcoded to $SOURCE_DIR"
 }
 
 ### Main function ###
@@ -33,27 +50,23 @@ main() {
     local target=""
 
     # Manage the command line arguments
-    ARGS=$(getopt -o 'h' --long 'help' -- "$@")
-    eval set -- "$ARGS"
-    unset ARGS
-
-    while true; do
-      case "$1" in
-        '-h'|'--help')
+    while getopts "h?" opt; do
+      case "$opt" in
+        h)
           usage
           exit 0
           ;;
-        '--')
-          shift
-          break
-          ;;
-        *)
-          exit_error 'Internal error!'
+        ?)
+          usage
+          exit 1
           ;;
       esac
     done
-    target=${@:$OPTIND:1}
+    shift $(expr $OPTIND - 1 )
+    target="$1"
     shift
+    [ "${1:-}" = "--" ] && shift
+    additional_args="${@}"
 
     if [[ "$target" == "" ]]; then
         echo_stderr "Please provide a target."
@@ -62,20 +75,36 @@ main() {
         exit 1
     fi
     target="${target%/}/"
-    log_file="$LOG_DIR/$(date +%Y%m%d_%H%M%S).log"
 
-    echo "> source:     $SOURCE_DIR"
-    echo "> target:     $target"
-    echo "> logging to: $log_file"
+    log_enabled=false
+    log_file="$LOG_DIR/$(date +%Y%m%d_%H%M%S).log"
+    log_arg=""
+    if [[ ! "$additional_args" =~ --dry-run|-n ]]; then
+        log_enabled=true
+        log_arg="--log-file=$log_file"
+    fi
+
+    echo "> source:        $SOURCE_DIR"
+    echo "> target:        $target"
+    if $log_enabled; then
+      echo "> logging to:    $log_file"
+    fi
+    echo "> rsync options: $additional_args"
 
     mkdir -p "$CONFIG_DIR" "$LOG_DIR"
 
     rsync -avP \
+      --stats \
+      --human-readable \
       --exclude-from ~/.config/sync_home/excludes \
-      --log-file="$log_file" \
+      $log_arg \
+      $additional_args \
       "$SOURCE_DIR" "$target"
 
-    echo "> logged to: $log_file"
+    if $log_enabled; then
+      echo "> logged to: $log_file"
+    fi
+    echo "> done"
 }
 
 main "$@"
