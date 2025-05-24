af5412c2c0e71211f4c250ff6cd85d517f6c867c
Theo Brigitte <theo.brigitte@gmail.com>
2025-05-24 11:40:19 +0200

    switch-theme: add desktop theme switch and fix tmux switch

--- .local/bin/switch-theme.sh
+++ .local/bin/switch-theme.sh
@@ -1,36 +1,82 @@
 #!/bin/bash
+# This script toggles between light and dark themes for base16-shell.
+# It assumes you have two themes: one for light and one for dark.
+# Usage: ./switch-theme.sh
 
 shopt -sq expand_aliases
+
+SHELL_THEME_LIGHT=~/.config/base16-shell/scripts/base16-tomorrow.sh
+SHELL_THEME_DARK=~/.config/base16-shell/scripts/base16-tomorrow-night.sh
+
+DESKTOP_THEME_LIGHT="WhiteSur-Light-solid"
+DESKTOP_THEME_DARK="WhiteSur-Dark-solid"
+
+ICON_THEME_LIGHT="WhiteSur-light"
+ICON_THEME_DARK="WhiteSur-dark"
 
 source "$BASE16_SHELL/profile_helper.sh"
 
 get_theme_name() {
-	theme_file="$(basename $1)"
-	theme_full_name="${theme_file%.*}"
-	theme_name="${theme_full_name#base16-}"
-	echo $theme_name
+  local theme_file="$(basename $1)"
+  local theme_full_name="${theme_file%.*}"
+  local theme_name="${theme_full_name#base16-}"
+  echo $theme_name
 }
 
-CURRENT_THEME_PATH="$(readlink -eq ~/.base16_theme)"
-CURRENT_THEME_NAME="$(get_theme_name "$CURRENT_THEME_PATH")"
+switch_shell_theme() {
+  local theme_name="$1"
+  local theme_path="$2"
 
-THEME_LIGHT=~/.config/base16-shell/scripts/base16-tomorrow.sh
-THEME_DARK=~/.config/base16-shell/scripts/base16-tomorrow-night.sh
+  export BASE16_THEME="$theme_name"
+  _base16 "$theme_path" "$theme_name"
+  echo "export BASE16_THEME=$BASE16_THEME" > ~/.config/shell/theme.sh
+}
 
-THEME_LIGHT_NAME="$(get_theme_name "$THEME_LIGHT")"
-THEME_DARK_NAME="$(get_theme_name "$THEME_DARK")"
+switch_tmux_theme() {
+  local theme_name="$1"
 
+  ln -fsr ~/.config/tmux/tinted-tmux/colors/base16-${theme_name}.conf ~/.config/tmux/theme.conf
+  tmux source ~/.config/tmux/theme.conf
+}
 
-if [ "$CURRENT_THEME_NAME" == "$THEME_DARK_NAME" ]; then
-		echo "Switching to light theme ($THEME_LIGHT_NAME)"
-		export BASE16_THEME="$THEME_LIGHT_NAME"
-		_base16 "$THEME_LIGHT" "$THEME_LIGHT_NAME"
-		tmux source ~/.config/tmux/tinted-tmux/colors/base16-${THEME_LIGHT_NAME}.conf
-else
-		echo "Switching to dark theme ($THEME_DARK_NAME)"
-		export BASE16_THEME="$THEME_LIGHT_NAME"
-		_base16 "$THEME_DARK" "$THEME_DARK_NAME"
-		tmux source ~/.config/tmux/tinted-tmux/colors/base16-${THEME_DARK_NAME}.conf
-fi
+switch_desktop_theme() {
+  local desktop_theme_name="$1"
+  local icon_theme_name="$2"
 
-echo "export BASE16_THEME=$BASE16_THEME" > ~/.config/shell/theme.sh
+  xfconf-query --channel xsettings --property /Net/ThemeName --create --type string --set "$desktop_theme_name"
+  xfconf-query --channel xfwm4 --property /general/theme --create --type string --set "$desktop_theme_name"
+  xfconf-query --channel xsettings --property /Net/IconThemeName --create --type string --set "$icon_theme_name"
+}
+
+main() {
+  local current_theme_path="$(readlink -eq ~/.base16_theme)"
+  local current_theme_name="$(get_theme_name "$current_theme_path")"
+
+  local shell_theme_light_name="$(get_theme_name "$SHELL_THEME_LIGHT")"
+  local shell_theme_dark_name="$(get_theme_name "$SHELL_THEME_DARK")"
+
+  local shell_theme_name=""
+  local shell_theme_path=""
+  local desktop_theme_name=""
+  local icon_theme_name=""
+
+  if [ "$current_theme_name" == "$shell_theme_dark_name" ]; then
+    echo "Switching to light theme ($shell_theme_light_name)"
+    shell_theme_name="$shell_theme_light_name"
+    shell_theme_path="$SHELL_THEME_LIGHT"
+    desktop_theme_name="$DESKTOP_THEME_LIGHT"
+    icon_theme_name="$ICON_THEME_LIGHT"
+  else
+    echo "Switching to dark theme ($shell_theme_dark_name)"
+    shell_theme_name="$shell_theme_dark_name"
+    shell_theme_path="$SHELL_THEME_DARK"
+    desktop_theme_name="$DESKTOP_THEME_DARK"
+    icon_theme_name="$ICON_THEME_DARK"
+  fi
+
+  switch_shell_theme "$shell_theme_name" "$shell_theme_path"
+  switch_tmux_theme "$shell_theme_name"
+  switch_desktop_theme "$desktop_theme_name" "$icon_theme_name"
+}
+
+main "$@"
